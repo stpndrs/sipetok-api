8a9f5204bbb009947ce3837fb05c1caa0084f235
rakhabram <rakha.bramantyo@gmail.com>
2026-04-27 12:36:03 +0700

    fix:utilis to utils

---
+++ Utils/PaymentState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace sipetok_api.Utilis
+{
+    public enum PaymentState
+    {
+        Pending,    
+        Processing, 
+        Success,    
+        Failed,     
+        Cancelled   
+    }
+}
