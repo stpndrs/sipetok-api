ec4d4fc228a1fa06d285627468fc9036c003476d
ravinhub <muhammadrizkyramadhan365@gmail.com>
2026-05-09 06:00:36 +0700

    refractor : add account role table driven

---
+++ Services/AccountRoleTableDriven.cs
@@ -0,0 +1,45 @@
+﻿namespace sipetok_api.Services
+{
+    public class AccountRoleTableDriven
+    {
+        string[] namaRole =
+        {
+            "ADMIN",
+            "TENANT",
+            "CUSTOMER"
+        };
+
+        int[] kodeRole =
+        {
+            1,
+            2,
+            3
+        };
+
+        public int GetKodeRole(string role)
+        {
+            for (int i = 0; i < namaRole.Length; i++)
+            {
+                if (namaRole[i] == role.ToUpper())
+                {
+                    return kodeRole[i];
+                }
+            }
+
+            return 0;
+        }
+
+        public string GetNamaRole(int kode)
+        {
+            for (int i = 0; i < kodeRole.Length; i++)
+            {
+                if (kodeRole[i] == kode)
+                {
+                    return namaRole[i];
+                }
+            }
+
+            return "ROLE TIDAK DITEMUKAN";
+        }
+    }
+}
