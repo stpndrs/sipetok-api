460213669644d6de93604011c4e9bf11d59d7b25
ravinhub <muhammadrizkyramadhan365@gmail.com>
2026-05-10 23:07:35 +0700

    refractor: Fix AccountStatusTableDriven

--- Services/AccountStatusTableDriven.cs
+++ Services/AccountStatusTableDriven.cs
@@ -2,43 +2,12 @@
 {
     public class AccountStatusTableDriven
     {
-        int[] statusAwal =
-        {
-            1,
-            0
-        };
-
-        string[] aksi =
-        {
-            "NONAKTIFKAN",
-            "AKTIFKAN"
-        };
-
-        int[] statusAkhir =
-        {
-            0,
-            1
-        };
-
-        public int GetNextStatus(int currentStatus, string action)
-        {
-            for (int i = 0; i < statusAwal.Length; i++)
-            {
-                if (statusAwal[i] == currentStatus && aksi[i] == action.ToUpper())
-                {
-                    return statusAkhir[i];
-                }
-            }
-
-            return currentStatus;
-        }
-
         public string GetStatusName(int status)
         {
             string[] namaStatus =
             {
-                "Tidak Aktif",
-                "Aktif"
+                 "INACTIVE",
+                  "ACTIVE"
             };
 
             return namaStatus[status];
