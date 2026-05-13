094b36b30fe37f2fd2d73c6cf2e1e5d6e91b8c28
ravinhub <muhammadrizkyramadhan365@gmail.com>
2026-05-13 07:25:08 +0700

    feat: add order status automata

---
+++ Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using sipetok_api.Utils;
+
+namespace sipetok_api.Services
+{
+    public class OrderService
+    {
+        private readonly Dictionary<(OrderState, OrderTrigger), OrderState> transitions =
+            new Dictionary<(OrderState, OrderTrigger), OrderState>
+        {
+            { (OrderState.WaitingForPayment, OrderTrigger.PaymentSucceeded), OrderState.ReadyForPickup },
+            { (OrderState.ReadyForPickup, OrderTrigger.PickedUp), OrderState.Completed },
+            { (OrderState.WaitingForPayment, OrderTrigger.CancelledByCustomer), OrderState.Cancelled }
+        };
+
+        public bool UpdateOrderStatus(OrderState currentOrderState, OrderTrigger trigger, PaymentState currentPaymentState, out OrderState nextOrderState)
+        {
+            nextOrderState = currentOrderState;
+
+            if (currentOrderState == OrderState.WaitingForPayment &&
+                trigger == OrderTrigger.PaymentSucceeded &&
+                currentPaymentState != PaymentState.Success)
+            {
+                return false;
+            }
+
+            if (transitions.TryGetValue((currentOrderState, trigger), out OrderState result))
+            {
+                nextOrderState = result;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Utils/OrderState.cs
@@ -0,0 +1,17 @@
+﻿namespace sipetok_api.Utils
+{
+    public enum OrderState
+    {
+        WaitingForPayment,
+        ReadyForPickup,
+        Completed,
+        Cancelled
+    }
+
+    public enum OrderTrigger
+    {
+        PaymentSucceeded,
+        PickedUp,
+        CancelledByCustomer
+    }
+}
