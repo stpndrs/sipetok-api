6d087fd02ca27d4961726ece2d56a09091a2e850
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2026-06-18 09:07:56 +0700

    Add UpdatedAt timestamp to Transactions and update model snapshot

---
+++ Migrations/20260618015838_UpdatedTransactionTimestampsField.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sipetok_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTransactionTimestampsField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Transactions",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Transactions");
+        }
+    }
+}
