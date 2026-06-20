974584d397be1c222f3d17517c63d1df79feb963
fdhill <muhammadfadhilahnaf11@gmail.com>
2026-06-20 20:58:10 +0700

    refactor(operational): filter data by auth and apply DRY/KISS principles

--- Repositories/OperationalRepository.cs
+++ Repositories/OperationalRepository.cs
@@ -1,13 +1,24 @@
-﻿using sipetok_api.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using sipetok_api.Data;
 using sipetok_api.Models;
 using sipetok_api.Repositories.Interfaces;
 
 namespace sipetok_api.Repositories
 {
-    public class OperationalRepository : GenericRepository<EggInventory>, IEggInventoryRepository
+    public class OperationalRepository : GenericRepository<Operational>, IOperationalRepository
     {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<Operational> _dbSet;
+
         public OperationalRepository(AppDbContext context) : base(context)
         {
+            _context = context;
+            _dbSet = _context.Set<Operational>();
+        }
+
+        public async Task<Operational> GetOperationalById(int id)
+        {
+            return await _dbSet.FirstOrDefaultAsync(o => o.Id == id);
         }
     }
 }
